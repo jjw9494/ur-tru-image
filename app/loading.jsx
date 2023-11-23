4915f59d8a53cf4eaccec99765859efef5e3c4cd
Tom Platt <thomasjplatt0107@gmail.com>
2023-11-23 08:26:19 +0000

    added loading page

---
+++ app/loading.jsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Loading Content...</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,46 +4,50 @@ import { useCallback, useState } from "react";
 import Feed from "./components/Feed";
 import NavController from "./components/NavController";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Loading from "./loading.jsx";
+import { Suspense } from "react";
 
 export default function Home() {
-	const router = useRouter();
-	const pathname = usePathname();
-	const searchParams = useSearchParams()!;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams()!;
 
-	const createQueryString = useCallback(
-		(value: string) => {
-			const params = new URLSearchParams(searchParams);
-			params.set("q", value);
+  const createQueryString = useCallback(
+    (value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set("q", value);
 
-			return params.toString();
-		},
-		[searchParams]
-	);
+      return params.toString();
+    },
+    [searchParams]
+  );
 
-	const searchValue = searchParams.get("q") || "";
+  const searchValue = searchParams.get("q") || "";
 
-	const [filterValue, setFilterValue] = useState("");
+  const [filterValue, setFilterValue] = useState("");
 
-	// Value of Filter input to be passed to Feed
-	const filterValueInput = (value: string): void => {
-		setFilterValue(value);
-	};
+  // Value of Filter input to be passed to Feed
+  const filterValueInput = (value: string): void => {
+    setFilterValue(value);
+  };
 
-	return (
-		<>
-			<NavController
-				searchValue={searchValue}
-				handleSearchInputValue={(input) => {
-					const queryString = createQueryString(input);
-					return router.replace(pathname + "?" + queryString, {
-						scroll: false,
-						shallow: false,
-					});
-				}}
-				filterValue={filterValue}
-				filterFunc={filterValueInput}
-			/>
-			<Feed filterValue={filterValue} searchValue={searchValue} />
-		</>
-	);
+  return (
+    <>
+      <NavController
+        searchValue={searchValue}
+        handleSearchInputValue={(input) => {
+          const queryString = createQueryString(input);
+          return router.replace(pathname + "?" + queryString, {
+            scroll: false,
+            shallow: false,
+          });
+        }}
+        filterValue={filterValue}
+        filterFunc={filterValueInput}
+      />
+      <Suspense fallback={<Loading />}>
+        <Feed filterValue={filterValue} searchValue={searchValue} />
+      </Suspense>
+    </>
+  );
 }
