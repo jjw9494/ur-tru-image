338fd646efe0468a3118a9d6b120013507268e56
jjw9494 <joejameswalsh94@gmail.com>
2023-11-26 17:19:52 +0000

    set up cypress e2e testing

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+//Remember to run dev before running tests - npm run open:cypress
+
+describe("template spec", () => {
+	it("Site renders", () => {
+		cy.visit("http://localhost:3000/");
+	});
+
+	it("Site renders", () => {
+		cy.visit("http://localhost:3000/");
+	});
+});
